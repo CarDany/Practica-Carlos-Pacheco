a49e1de9f90affb313042c95e1341c60143e9f76
Carlos Pacheco <danielpacheco090310@gmail.com>
2026-05-27 08:33:10 -0600

    FIX: Corregir los errores del último print y los tres for

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j < (5 + 1) - i; j++) {
-
+                System.out.print(" ");
             }
 
-            for (int x = 1; x < (i + 1) - 1; x++) {
-
+            for (int x = 1; x <= (i * 2) - 1; x++) {
+                System.out.print("*");
             }
+            System.out.println("");
         }
     }
 }
