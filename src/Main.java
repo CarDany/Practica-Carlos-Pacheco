ac551e4ab74e6d81d5b211aa94499bd4beca0e74
Carlos Pacheco <danielpacheco090310@gmail.com>
2026-05-27 08:23:49 -0600

    FEAT: Crear la base del programa

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < (5 + 1) - i; j++) {
+
+            }
+
+            for (int x = 1; x < (i + 1) - 1; x++) {
+
+            }
+        }
     }
 }
